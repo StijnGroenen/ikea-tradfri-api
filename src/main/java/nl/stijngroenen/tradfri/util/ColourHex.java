167abb4a63389113dd9915970dc65809d8dc99cb
Stijn Groenen <stijn.groenen@gmail.com>
2020-03-11 20:16:22 +0100

    Added hex colour constants

---
+++ src/main/java/nl/stijngroenen/tradfri/util/ColourHex.java
@@ -0,0 +1,32 @@
+package nl.stijngroenen.tradfri.util;
+
+public class ColourHex {
+
+    // Colour Temperature
+    public static final String WHITE = "f5faf6";
+    public static final String WARM = "f1e0b5";
+    public static final String GLOW = "efd275";
+
+    // Colour RGB
+    public static final String BLUE = "4a418a";
+    public static final String LIGHT_BLUE = "6c83ba";
+    public static final String SATURATED_PURPLE = "8f2686";
+    public static final String LIME = "a9d62b";
+    public static final String LIGHT_PURPLE = "c984bb";
+    public static final String YELLOW = "d6e44b";
+    public static final String SATURATED_PINK = "d9337c";
+    public static final String DARK_PEACH = "da5d41";
+    public static final String SATURATED_RED = "dc4b31";
+    public static final String COLD_SKY = "dcf0f8";
+    public static final String PINK = "e491af";
+    public static final String PEACH = "e57345";
+    public static final String WARM_AMBER = "e78834";
+    public static final String LIGHT_PINK = "e8bedd";
+    public static final String COOL_DAYLIGHT = "eaf6fb";
+    public static final String CANDLELIGHT = "ebb63e";
+    public static final String WARM_GLOW = "efd275";
+    public static final String WARM_WHITE = "f1e0b5";
+    public static final String SUNRISE = "f2eccf";
+    public static final String COOL_WHITE = "f5faf6";
+
+}
---
+++ src/main/java/nl/stijngroenen/tradfri/util/ColourTemperatureHex.java
@@ -0,0 +1,9 @@
+package nl.stijngroenen.tradfri.util;
+
+public class ColourTemperatureHex {
+
+    public static final String WHITE = "f5faf6";
+    public static final String WARM = "f1e0b5";
+    public static final String GLOW = "efd275";
+
+}
