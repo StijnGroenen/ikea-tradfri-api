accaef22d0955191c6ae2dc275b736106cc09c1f
Stijn Groenen <stijn.groenen@gmail.com>
2020-06-08 19:25:33 +0200

    Added device type enumerator

---
+++ src/main/java/nl/stijngroenen/tradfri/device/DeviceType.java
@@ -0,0 +1,14 @@
+package nl.stijngroenen.tradfri.device;
+
+/**
+ * The enumerator that contains IKEA TRÅDFRI device types
+ * @author Stijn Groenen
+ * @version 1.1.0
+ */
+public enum DeviceType {
+    UNKNOWN,
+    LIGHT,
+    PLUG,
+    REMOTE,
+    MOTION_SENSOR,
+}
